6cccc8dd68930d8e44c9ff915846963af6be20d1
Ivan Zhang <Ivan.Zhang@monash.edu>
2020-01-13 13:46:58 +1100

    random geom gen complete

--- abaqus_working_space/abaqus_out/run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-REM call "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2017.6.270\windows\bin\ifortvars.bat" intel64
-REM call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-
-start "abaqus run" "C:\SIMULIA\CAE\2019\win_b64\code\bin\ABQLauncher.exe" -job dflux_experiment -user udfluxxx
--- working_with_meep/read_h5.py
+++
@@ -1,2 +0,0 @@
-import h5py
-f = h5py.File('ez.h5', 'r')
